8087fae53cfad119f45f79d161723254c8d5f56e
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-27 10:44:25 +0100

    Update scraper.

--- parsers/products_search.rb
+++ parsers/products_search.rb
@@ -108,7 +108,7 @@ products.each_with_index do |product, i|
       PRODUCT_NAME: product['name'],
       EAN: product['sku'],
       PRODUCT_DESCRIPTION: "",
-      PRODUCT_MAIN_IMAGE_URL:"https:"+product['images']['list'].first[1],
+      PRODUCT_MAIN_IMAGE_URL:"https:"+product['images']['list']["200"],
       PRODUCT_ITEM_SIZE: item_size,
       PRODUCT_ITEM_SIZE_UOM: uom,
       PRODUCT_ITEM_QTY_IN_PACK: in_pack,
