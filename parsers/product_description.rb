b5ebc8556dc1cfd3b4bf383e4612de7c3084b578
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-09 15:00:29 +0100

    update scraper.

--- parsers/product_description.rb
+++ parsers/product_description.rb
@@ -1,13 +1,16 @@
 data = JSON.parse(content)
 products_details = page['vars']['product_details']
-description = data['data']['product']['content']['description']
-data['data']['product']['features'].each do |feature|
+
+description = data['data']['product']['content']['description'] rescue ''
+features = data['data']['product']['features'] rescue []
+features.each do |feature|
 
 description = description +' . '+feature['label']+':'+feature['value']
 
 
 end
-availability = (data['data']['product']['isAvailable']==true)?"1":""
+availability = data['data']['product']['isAvailable'] rescue  false
+availability = (availability==true)?"1":""
 products_details[:PRODUCT_DESCRIPTION]=description
 products_details[:IS_AVAILABLE] = availability
 
