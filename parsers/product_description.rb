6490e83b30619f5b248a6b15c49bc209e272d4e0
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-09 15:16:32 +0100

    update scraper.

--- parsers/product_description.rb
+++ parsers/product_description.rb
@@ -3,12 +3,16 @@ products_details = page['vars']['product_details']
 
 description = data['data']['product']['content']['description'] rescue ''
 features = data['data']['product']['features'] rescue []
+
+description=(description.nil?)?'':description
 features.each do |feature|
+
 
 description = description +' . '+feature['label']+':'+feature['value']
 
-
 end
+
+
 availability = data['data']['product']['isAvailable'] rescue  false
 availability = (availability==true)?"1":""
 products_details[:PRODUCT_DESCRIPTION]=description
